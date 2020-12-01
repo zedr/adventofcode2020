686fc5fcd36e89268105732552a5b2423de3d66b
Rigel Di Scala <zedr@users.noreply.github.com>
2020-12-01 19:05:48 +0100

    Create day1.js
    Add day1 for Nodejs

---
+++ day1.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readFile('input.txt', 'ascii', (err, data) => {
+  const numbers = Array.from(data.split('\n'), n => parseInt(n));
+  
+  numbers.forEach((num1, idx) => {
+    numbers.slice(idx).forEach(num2 => {
+      if (num1 + num2 === 2020) {
+        console.log([num1, num2]);
+        console.log(num1 * num2);
+        return;
+      }
+    })
+  })
+});
