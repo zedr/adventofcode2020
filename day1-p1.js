18d0c8ddc7db825469a4512794b1a2b909458650
Rigel Di Scala <zedr@users.noreply.github.com>
2020-12-02 09:02:34 +0100

    Update and rename day1.js to day1-p1.js

--- day1-p1.js
+++ day1-p1.js
@@ -4,7 +4,7 @@ fs.readFile('input.txt', 'ascii', (err, data) => {
   const numbers = Array.from(data.split('\n'), n => parseInt(n));
   
   numbers.forEach((num1, idx) => {
-    numbers.slice(idx).forEach(num2 => {
+    numbers.slice(idx + 1).forEach(num2 => {
       if (num1 + num2 === 2020) {
         console.log([num1, num2]);
         console.log(num1 * num2);
